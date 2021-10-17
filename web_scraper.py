1f9c1246b0e81f8a668da038eaef9218b45723e4
Patryk <musial.patryk10@gmail.com>
2021-10-17 17:06:03 +0200

    Make all properties private

--- web_scraper.py
+++ web_scraper.py
@@ -7,15 +7,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 class WebScraper:
     def __init__(self, url, print_suffix):
-        self.url = url
-        self.print_suffix = print_suffix
-        self.browser = self.__get_webdriver()
-        self.all_prices = []
+        self.__url = url
+        self.__print_suffix = print_suffix
+        self.__browser = self.__get_webdriver()
+        self.__all_prices = []
 
     def print_average(self, page_count):
-        self.all_prices = []
+        self.__all_prices = []
         self.__get_prices_from_pages(page_count)
-        print('Average cost per meter is:', "%.2f" % self.__compute_average(self.all_prices), self.print_suffix)
+        print('Average cost per meter is:', "%.2f" % self.__compute_average(self.__all_prices), self.__print_suffix)
 
     def __get_webdriver(self):
         options = Options()
@@ -25,8 +25,8 @@ class WebScraper:
         return webdriver.Chrome(service=s, options=options)
 
     def __get_all_descriptions(self, url):
-        self.browser.get(url)
-        apartments = self.browser.find_elements(By.TAG_NAME, 'article')
+        self.__browser.get(url)
+        apartments = self.__browser.find_elements(By.TAG_NAME, 'article')
 
         descriptions = []
         for apartment in apartments:
@@ -38,7 +38,7 @@ class WebScraper:
 
     def __remove_suffix_from_price(self, price_text):
         removed_whitespaces = "".join(price_text.split())
-        clear_price = removed_whitespaces.replace(self.print_suffix, '')
+        clear_price = removed_whitespaces.replace(self.__print_suffix, '')
         if len(clear_price) == 0:
             return None
         return float(clear_price)
@@ -61,5 +61,5 @@ class WebScraper:
     def __get_prices_from_pages(self, count):
         for i in range(1, count + 1):
             print('Crawling pagination page: ' + str(i))
-            apartments_descriptions = self.__get_all_descriptions(self.url + str(i))
-            self.all_prices.extend(self.__get_prices(apartments_descriptions))
+            apartments_descriptions = self.__get_all_descriptions(self.__url + str(i))
+            self.__all_prices.extend(self.__get_prices(apartments_descriptions))
